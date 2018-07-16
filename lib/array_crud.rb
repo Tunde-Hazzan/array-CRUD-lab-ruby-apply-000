0be50caa0d5f7b2659dc9cb83e0cf2aab2ccf6d1
Tunde Hazzan <t.hazzan@ctrlmetrics.com>
2018-07-16 19:48:23 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,23 +8,20 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [1,2,3,4]
-  element = 5
-  return
-  array << element
+    array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
-  [1,2,3,4].unshift(5)
+  array.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  [1,2,3,4] 
+  array.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  [1,2,3,4] 
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
